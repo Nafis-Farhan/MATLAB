23a4ba42d9126ce1783d9277322098e8462f892c
Nafis Farhan <nf5009@bard.edu>
2023-10-18 00:24:43 -0400

    add solve

---
+++ solve.m
@@ -0,0 +1,14 @@
+function [t,x] = solve(k1)
+
+[t,x] = ode45(@(time,x_pos) model(time,x_pos,k1),[0 .5],[0; 0]);
+N = length(t);
+
+dt= .5/N;
+
+for i=1:N-1
+    t_old = t(i+1);
+    t(i+1) = t(i)+dt;
+    x_old = x(i+1,:);
+    x(i+1,1) = x(i,1)+((t(i+1)-t(i))*(x_old(1)-x(i,1))/(t_old-t(i)));
+    x(i+1,2) = x(i,2)+((t(i+1)-t(i))*(x_old(2)-x(i,2))/(t_old-t(i)));
+end
