8fa310006c67fca6d6b5050c61c368a6b9364c70
Nafis Farhan <nf5009@bard.edu>
2023-10-19 14:45:47 -0400

    fix

--- solve.m
+++ solve.m
@@ -9,19 +9,20 @@ for i = 1:n-1
     end
 end
 N = fix(.5/dt);
-t_new = linespace(0,.5,N);
+t_new = linspace(0,.5,N);
 x_new = zeros(N,1);
 j=2;
-x_new(1)=x(0,1);
+x_new(1)=x(1,1);
 for i=1:N-1
-    if t_new(i)>t(j) && j < n
+    if  j < n && t_new(i)>t(j) 
         j=j+i;
-        continue;
     end
-    x_new(i+1) = x_new(i) + m*
+    j
+    x_new(i+1) = x_new(i) + ((x(j,1)-x(j-1,1))/(t(j)-t(j-1)))*(t_new(i+1)-t_new(i));
         
 end
 
-%plot(t,x(:,1))
+plot(t,x(:,1))
+plot(t_new,x_new)
 
 %sound(x(:,1),1/dt)
